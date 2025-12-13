1a32544b70585c318c31ef58fd928c81584063f6
quyen281009 <quyenthong45@gmail.com>
2025-12-13 16:45:52 +0700

    3D Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductViewer from "./components/ProductViewer";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 };
